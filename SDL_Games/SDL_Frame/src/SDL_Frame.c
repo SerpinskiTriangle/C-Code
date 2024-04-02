4446df05e7fc30ac70e9d2b75a34b400ef0d4416
Ivan Kolesov <kolesov.id@gmail.com>
2024-04-01 22:38:01 -0400

    added rudimentary automated entity creation and rendering

--- SDL_Games/SDL_Frame/src/SDL_Frame.c
+++ SDL_Games/SDL_Frame/src/SDL_Frame.c
@@ -5,6 +5,7 @@
 //this looks atrocious because i want a working demo of everything before i make it scalable to innumerable entities
 
 #define CLASS_COUNT 3
+#define MAX_ENTITIES 5
 
 
 int8_t still;
@@ -34,15 +35,25 @@ struct entity{
     int width;
     int faceAngleDeg;
     float moveAngleRad;
-};
+}EG;
+
 
 struct entity player = {0,0,0,0,playerHeight, playerWidth,0,0};
 struct entity test = {300,300,0,0,100,100,0,0};
 
-struct entity ***gameEntities[CLASS_COUNT];    
+struct entity ***gameEntities[CLASS_COUNT];
+
+int allocTable[CLASS_COUNT][1] = {
+    {0}, //walls
+    {0}, //entities
+    {0}  //projectiles
+};
+
 /* ok please dont laugh i promise this is for memory efficiency
  * all entities are in a hierarchy (class > type > individual entity)
- *
+ * 1 type per class for now (pointers will be implemented later for different numbers of types for each class)(i think)
+ * allocTable holds the amount of entity pointers for each type for each class
+ * new pointers to entities will always be placed at that index
  */
 
 
@@ -52,6 +63,15 @@ void drawRect(struct SDL_Renderer *renderer, int x, int y, int h, int w);
 
 int collideStatus(float ent1X, float ent1Y, int ent1H, int  ent1W, float ent2X, float ent2Y, int ent2H, int ent2W);
 
+void summonEntity(float xPos, float yPos, float xSpeed, float ySpeed, int height, int width, int faceAngleDeg, float moveAngleRad, int class, int type){
+    struct entity *entity = (struct entity*)malloc(sizeof(struct entity));
+    *entity = (struct entity){xPos,yPos,xSpeed,ySpeed,height,width,faceAngleDeg,moveAngleRad};
+    gameEntities[class][type][allocTable[class][type]] = entity;
+    allocTable[class][type]++;
+}
+
+void drawEntHitbox(struct SDL_Renderer *renderer, struct entity entity);
+
 
 int main(){
     SDL_Init(SDL_INIT_VIDEO);
@@ -60,8 +80,18 @@ int main(){
     SDL_SetRenderDrawColor(state.renderer,0,0,0,255);
     SDL_RenderClear(state.renderer);
 
-    
+    for (int i = 0; i <= CLASS_COUNT - 1; i++){
+        gameEntities[i] = (struct entity***)malloc(sizeof(struct entity**));
+        for (int j = 0; j <= 5; j++){
+            *gameEntities[i] = (struct entity**)malloc(sizeof(struct entity*) * MAX_ENTITIES);
+        }
+    }
 
+
+    //we love castin spells
+    summonEntity(100,100,0,0,350,50,0,0,0,0);
+    summonEntity(400,100,0,0,350,50,0,0,1,0);
+    summonEntity(150,400,0,0,50,250,0,0,1,0);
     while (state.running){
         SDL_SetRenderDrawColor(state.renderer,0,0,0,255);
         SDL_RenderClear(state.renderer);
@@ -72,7 +102,6 @@ int main(){
             }
         }
         keyboardState = SDL_GetKeyboardState(NULL);
-
         if (!(keyboardState[SDL_SCANCODE_W] - keyboardState[SDL_SCANCODE_S] || keyboardState[SDL_SCANCODE_A] - keyboardState[SDL_SCANCODE_D])){
             still = 1;
             player.xSpeed = 0;
@@ -92,6 +121,7 @@ int main(){
             player.ySpeed = 0;
         }
 
+        
         player.xPos += 5 * player.xSpeed;
         player.yPos += 5 * player.ySpeed;
         //debug section
@@ -101,6 +131,15 @@ int main(){
         collide = collideStatus(player.xPos,player.yPos,player.height,player.width,test.xPos,test.yPos,test.height,test.width);
 
         SDL_SetRenderDrawColor(state.renderer,255,255 * !collide,255 * !collide,255);
+
+        
+        for(int class = 0; class <= CLASS_COUNT - 1; class++){
+            for(int entity = 0; entity < allocTable[class][0]; entity++){
+                drawEntHitbox(state.renderer,*gameEntities[class][0][entity]);
+            }
+        }
+        
+        //drawEntHitbox(state.renderer,*gameEntities[0][0][0]);
         drawEntHitbox(state.renderer,player);
         drawEntHitbox(state.renderer,test);
         SDL_RenderPresent(state.renderer);
@@ -108,6 +147,15 @@ int main(){
         
         frame++;
     }
+    //entity cleanup
+    
+    for(int class = 0; class <= CLASS_COUNT - 1; class++){
+        for(int entity = 0; entity < allocTable[class][0]; entity++){
+            free(gameEntities[class][0][entity]);
+        }
+    }
+    
+
     printf("\n");
     SDL_DestroyRenderer(state.renderer);
     SDL_DestroyWindow(state.window);
@@ -133,6 +181,6 @@ int collideStatus(float ent1X, float ent1Y, int ent1H, int  ent1W, float ent2X, 
     );
 }//its not unreadable, its optimized (trust)
 
-void drawEntHitbox(struct SDL_renderer *renderer, struct entity entity){
+void drawEntHitbox(struct SDL_Renderer *renderer, struct entity entity){
     drawRect(renderer,entity.xPos,entity.yPos,entity.height,entity.width);
 }
