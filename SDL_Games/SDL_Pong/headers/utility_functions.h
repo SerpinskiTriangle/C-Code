0c31c5dd13ff51966070d5fc3fe13c7d90796b18
Ivan Kolesov <kolesov.id@gmail.com>
2024-04-29 12:13:33 -0400

    naming scheme normalized, among other things

---
+++ SDL_Games/SDL_Pong/headers/utility_functions.h
@@ -0,0 +1,7 @@
+#ifndef UTILITY_FUNCTIONS_H
+    #define UTILITY_FUNCTIONS_H
+    #include <SDL.h>
+    void resetPos();
+    void renderLetter(int x, int y, int digit);
+    void drawRect(int x, int y, int h, int w);
+#endif
