914833d16f5b56d00d9f2aa0ae8d82c8ce7908e5
Ivan Kolesov <kolesov.id@gmail.com>
2024-08-02 21:14:49 -0400

    quit now allows window to be closed

--- SDL_Games/SDL_3D_Test/src/quit.c
+++ SDL_Games/SDL_3D_Test/src/quit.c
@@ -1,6 +1,7 @@
 #include <SDL.h>
 #include "quit.h"
 #include "main.h"
+#include "SDL_state.h"
 
 
 SDL_Event currentSDL_event;
@@ -9,6 +10,8 @@ void checkQuit(){
     while (SDL_PollEvent(&currentSDL_event)){
             if (currentSDL_event.type == SDL_QUIT){
                 running = 0;
+                SDL_DestroyWindow(state.window);
+                SDL_Quit();
         }
     }
 }
